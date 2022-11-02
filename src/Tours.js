f8eba888b97a82ee65af85b5af04e42649ed1323
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-11-02 23:48:57 +0100

    done

--- src/Tours.js
+++ src/Tours.js
@@ -1,39 +1,19 @@
-import React, { useState } from 'react';
-import Loading from './Loading';
-
-const Tours = ({places}) => {
-
-  const [read, setRead] = useState(false)
-
-
-
-
-  return (<div>
-    {places.map((item, index)=>{
-
-const {id, image, info, name,price} = item
-      return(
-        <div key={index} style={{display:'flex' ,justifyContent:'center', alignItems:'center' ,flexDirection:'column' }}>
-          <img src={image} alt="" />
-          <div style={{display:"flex" , justifyContent:'space-between'}}>
-            <p>{name}</p>
-            <p>{price}</p>
-          </div>
-          <div style={{width:'75%'}}>
-            {read  ? info : `${info.substring(0,200)}...`}
-            
-            <button onClick={()=>setRead(!read)}>{read ? 'show less' : 'read more'}</button>
-            </div>
-
-          
-          </div>
-
-
-      )
-    })}
-   
-
-  </div>);
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
